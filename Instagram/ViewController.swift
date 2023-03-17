161cbe80b805ee06d22544530d824b9dc0b7dd79
toru-kawasaki <toru_kawasaki@jrass.jp>
2023-03-17 15:49:40 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by PC-SYSKAI556 on 2023/03/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
